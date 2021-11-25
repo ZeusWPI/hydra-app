8ddd5fd9336ac8fce3c648ab2e503c0b1dc742ab
jay-tux <sys.jonas@yahoo.com>
2021-11-25 21:18:39 +0100

    Initial version of association model

---
+++ src/api/models/association.model.ts
@@ -0,0 +1,9 @@
+export interface Association {
+  abbrev: string; // you need to abbrev. abbrev., no?
+  name: string;
+  paths: string[];
+  desc: string;
+  email: string;
+  logo: string;
+  website: string;
+}
