af006b5fed399c86d613566390c2e21225a92fb1
jay-tux <sys.jonas@yahoo.com>
2021-11-25 22:42:13 +0100

    Updates/fixed association/event model to match JSON

--- src/api/models/association.model.ts
+++ src/api/models/association.model.ts
@@ -1,9 +1,47 @@
 export interface Association {
-  abbrev: string; // you need to abbrev. abbrev., no?
-  name: string;
-  paths: string[];
-  desc: string;
+  abbreviation: string;
+  description?: string;
   email: string;
-  logo: string;
-  website: string;
+  logo?: string;
+  name: string;
+  path: string[];
+  website?: string;
+}
+
+export interface AssociationList {
+  associations: Association[];
+}
+
+export interface AEvent {
+  address?: string;
+  advertise: boolean;
+  association: string;
+  description?: string;
+  end_time: string;
+  id: number;
+  infolink?: string;
+  location: string;
+  start_time: string; // what is this ordering?
+  sync_data?: Object; // seems to be null everywhere so idk?
+  terrain: string;
+  title: string;
+  type: string;
+}
+
+export interface EventMeta {
+  authenticated: boolean;
+  params: Object; // we don't seem to be authenticated, so let's just ignore this one?
+}
+
+export interface EventPage {
+  entries: AEvent[];
+  page_number: number;
+  page_size: number;
+  total_entries: number;
+  total_pages: number;
+}
+
+export interface EventList {
+  meta: EventMeta;
+  page: EventPage;
 }
