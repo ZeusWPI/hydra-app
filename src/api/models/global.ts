635c7e070a56c015a9275ff9eae06c87d6f73856
jay-tux <sys.jonas@yahoo.com>
2021-11-25 23:09:36 +0100

    Documented semantic type aliases

--- src/api/models/global.ts
+++ src/api/models/global.ts
@@ -1,6 +1,29 @@
+/**
+ * URLs are strings
+ */
 type urlString = string;
+
+/**
+ * Addresses are strings
+ */
 type addressString = string;
+
+/**
+ * Dates/date times are strings
+ */
 type dateString = string;
+
+/**
+ * Email addresses are strings
+ */
 type mailString = string;
+
+/**
+ * Phone numbers are strings
+ */
 type phoneString = string;
+
+/**
+ * HTML blocks are strings
+ */
 type htmlString = string;
