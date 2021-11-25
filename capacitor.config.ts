efa1061ae8e96432e3a4ebf3a6a3230b5bfe8941
maartenvn <maartenvn.eu@gmail.com>
2021-11-25 19:28:14 +0100

    chore: initialize ionic app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hydra',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
